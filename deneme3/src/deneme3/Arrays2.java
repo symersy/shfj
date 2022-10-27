b45480cbcdde50d6174b795f7d88c102472029d9
ŞEYMA <EYMA@10.48.253.122>
2022-10-27 10:47:24 +0300

    3.gonderi

--- deneme3/src/deneme3/Arrays2.java
+++ deneme3/src/deneme3/Arrays2.java
@@ -3,7 +3,17 @@ package deneme3;
 public class Arrays2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final int ARRAY_LENGTH=10;
+		int[] array=new int[ARRAY_LENGTH];
+		
+		for(int counter=0;counter<array.length;counter++) {
+			array[counter]=2+2*counter;
+			
+		}
+  System.out.printf("%s%8s%n","Index","Value");
+  for(int counter=0;counter<array.length;counter++) {
+	  System.out.printf("%s%8s%n",counter,array[counter]);
+  }
 
 	}
 
